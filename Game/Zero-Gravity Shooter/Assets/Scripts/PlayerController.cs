81f81105a307168e735d13bcbbc8ff6ada38f328
Jacob <jacob.nance97@gmail.com>
2016-09-12 22:31:13 -0500

    Added a player controller. Created elements for a gun and player rotator, these are incomplete.

---
+++ Game/Zero-Gravity Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontalSpeed = 30.0f;
+    public float verticalSpeed = 30.0f;
+    //public float rotationSpeed = 100.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    //private float rotate = 0.0f;
+
+    void Update()
+    {
+        yaw += horizontalSpeed * Input.GetAxis("Mouse X");
+        pitch -= verticalSpeed * Input.GetAxis("Mouse Y");
+        //rotate += rotationSpeed * Time.deltaTime;
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        //if (Input.GetKey(KeyCode.Q))                              //not working quite right, putting a pin in it
+        //{
+        //    transform.eulerAngles = new Vector3(0, 0, rotate);
+        //}
+
+        //if (Input.GetKey(KeyCode.E))
+        //{
+        //    transform.eulerAngles = new Vector3(0, 0, -rotate);
+        //}
+
+    }
+}
---
+++ Game/Zero-Gravity Shooter/Assets/Scripts/RaycastShoot.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastShoot : MonoBehaviour {
+
+    public int gunDamage = 1;
+    public float fireRate = .25f;
+    public float weaponRange = 50f;
+    public float hitForce = 25f;
+    public Transform gunEnd;
+
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(.01f);
+    //private AudioSource gunAudio;
+    private LineRenderer laserLine;  //should be replaced with a bullet texture
+    private float nextFire; 
+
+	void Start ()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        fpsCam = GetComponentInParent<Camera>();
+        //gunAudio = GetComponent<>();
+	}
+	
+
+	void Update ()
+    {
+	    if (Input.GetButtonDown ("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+            StartCoroutine(ShotEffect());
+
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+
+            laserLine.SetPosition(0, gunEnd.position);
+
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+            }
+        }
+	}
+
+    private IEnumerator ShotEffect()
+    {
+        //gunAudio.Play();
+
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
+}
