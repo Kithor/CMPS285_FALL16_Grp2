37f302259a5e409a02f1a86fefb3d30945447526
Jacob Nance <jacob.nance97@gmail.com>
2016-09-15 20:53:03 -0500

    -Removed raycasting for weapons +Added a projectile generation script for weapons

--- Game/Zero-Gravity Shooter/Assets/Scripts/PlayerController.cs
+++ Game/Zero-Gravity Shooter/Assets/Scripts/PlayerController.cs
@@ -4,31 +4,30 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-    public float horizontalSpeed = 30.0f;
-    public float verticalSpeed = 30.0f;
+    public float horizontalSpeed = 10.0f;
+    public float verticalSpeed = 10.0f;
     //public float rotationSpeed = 100.0f;
 
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
-    //private float rotate = 0.0f;
+    private float yaw;
+    private float pitch;
+    //private float rotate;
 
     void Update()
     {
         yaw += horizontalSpeed * Input.GetAxis("Mouse X");
         pitch -= verticalSpeed * Input.GetAxis("Mouse Y");
-        //rotate += rotationSpeed * Time.deltaTime;
-
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        //rotate = rotationSpeed * Time.deltaTime;
 
         //if (Input.GetKey(KeyCode.Q))                              //not working quite right, putting a pin in it
         //{
-        //    transform.eulerAngles = new Vector3(0, 0, rotate);
+        //    transform.Rotate(0, 0, rotate);
         //}
 
         //if (Input.GetKey(KeyCode.E))
         //{
-        //    transform.eulerAngles = new Vector3(0, 0, -rotate);
+        //    transform.Rotate(0, 0, -rotate);
         //}
 
+        transform.eulerAngles = new Vector3(pitch, yaw, 0);
     }
 }
---
+++ Game/Zero-Gravity Shooter/Assets/Scripts/WeaponFire.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponFire : MonoBehaviour
+{
+    public Rigidbody projectile;
+    private float fireRate = 100;
+	
+	void Update ()
+    {   //Run when button pressed
+	    if (Input.GetButtonDown("Fire1")) 
+        {   //Generate projectile
+            Rigidbody generateProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+
+            //Move Projectile
+            generateProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, fireRate));
+        }
+	}
+}
