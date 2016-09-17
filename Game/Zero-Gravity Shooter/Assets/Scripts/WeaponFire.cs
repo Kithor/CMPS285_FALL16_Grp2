2c822b36e4188e00ac195144b8ce450a73e3d281
Jacob <jacob.nance97@gmail.com>
2016-09-17 09:58:50 -0500

    +Added reload to weapon +Added weapon inertia generation

--- Game/Zero-Gravity Shooter/Assets/Scripts/WeaponFire.cs
+++ Game/Zero-Gravity Shooter/Assets/Scripts/WeaponFire.cs
@@ -4,16 +4,39 @@ using System.Collections;
 public class WeaponFire : MonoBehaviour
 {
     public Rigidbody projectile;
-    private float fireRate = 100;
-	
-	void Update ()
-    {   //Run when button pressed
-	    if (Input.GetButtonDown("Fire1")) 
-        {   //Generate projectile
-            Rigidbody generateProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
 
-            //Move Projectile
-            generateProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, fireRate));
+
+    int shotsFired = 0;             //amount of shots fired
+    int maxFired = 33;              //amount that can be fired
+
+    private Rigidbody player;
+    public float bulletSpeed = 100; //will be private, public for easy testing
+  //public float fireRate = 0;      //private
+    public float weaponRange = 10;  //private
+
+    void Start()
+    {
+        player = GetComponentInParent<Rigidbody>();
+    }
+
+	void Update ()
+    {   //change value of firing when left click pressed
+
+        if (Input.GetButton("Fire1") && shotsFired < maxFired)
+        {
+            Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, Quaternion.identity) as Rigidbody;   //Generate projectile
+            instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, bulletSpeed));                     //Move Projectile                                                                                                                                  
+            player.AddForce(-instantiatedProjectile.velocity);                                                                  //Move Parent Object (player)
+
+            shotsFired++;
+
         }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            shotsFired = 0;
+        }
+
+    //Destroy(GameObject.FindGameObjectWithTag("Bullet"), 5);
+
 	}
 }
