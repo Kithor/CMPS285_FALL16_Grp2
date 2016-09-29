ac9e07cf84d3d5751f60f57485b2905fee26b4a0
Jacob <jacob.nance97@gmail.com>
2016-09-29 07:48:37 -0500

    +Added controls to reorient player

--- Game/Zero-Gravity Shooter/Assets/Scripts/PlayerController.cs
+++ Game/Zero-Gravity Shooter/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,29 @@ public class PlayerController : MonoBehaviour
     public Rigidbody player;
     public float horizontalSpeed = 10.0f;
     public float verticalSpeed = 10.0f;
+    public float rotateSpeed = 70.0f;
 
     private float yaw;
     private float pitch;
+    private float rotation;
 
     void Update()
     {
         yaw = horizontalSpeed * Input.GetAxis("Mouse X");
         pitch = verticalSpeed * Input.GetAxis("Mouse Y");
+        rotation = rotateSpeed * Time.deltaTime;
 
-        //transform.localEulerAngles = new Vector3(pitch, yaw, 0);
         transform.Rotate(Vector3.up, yaw);
         transform.Rotate(Vector3.left, pitch);
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.forward, -rotation);
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.forward, rotation);
+        }
     }
 
     void OnCollisionEnter(Collision col){}
