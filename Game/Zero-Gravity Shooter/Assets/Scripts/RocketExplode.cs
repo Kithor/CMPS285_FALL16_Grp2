c43a2d9eff8fdd4f01d4b2cb075892ddb862715f
Mtmisuraca <mtmisuraca@gmail.com>
2016-11-26 19:34:48 -0600

    Uploaded the correct Rocket Launcher
    WeaponFire is the gun, RocketExplode goes on the bullet.

---
+++ Game/Zero-Gravity Shooter/Assets/Scripts/RocketExplode.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketExplode : MonoBehaviour
+{
+    public float force = 50.0f;
+    public float radius = 5.0f;
+    public float upwardsModifier = 0.0f;
+    public float weaponDamage = 40;
+    public ForceMode forceMode;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            foreach (Collider col in Physics.OverlapSphere(transform.position, radius))
+            {   
+                if (col.tag == "Player")
+                {
+                    var enemy = col.GetComponent<Rigidbody>();                             //Create variable for the enemy rigidbody
+                    enemy.AddForce(transform.TransformDirection(new Vector3(0, 0, (force))));    //Apply force to enemy player
+
+                    var enemyHealth = col.GetComponent<PlayerHealth>();                    //Create variable for the enemy health
+                    enemyHealth.hitDelay = enemyHealth.hitTimer + Time.time;                        //Start the health regen clock for enemy player
+                    enemyHealth.health -= weaponDamage;                                             //Reduce enemy health by weaponDamage
+                }
+                if (col.GetComponent<Rigidbody>() != null)
+                {
+                    col.GetComponent<Rigidbody>().AddExplosionForce(force, transform.position, radius, upwardsModifier, forceMode);
+                }
+            }
+
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (Collider col in Physics.OverlapSphere(transform.position, radius))
+        {
+            if (col.GetComponent<Rigidbody>() != null)
+            {
+                col.GetComponent<Rigidbody>().AddExplosionForce(force, transform.position, radius, upwardsModifier, forceMode);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
--- Game/Zero-Gravity Shooter/Assets/Scripts/WeaponFire.cs
+++ Game/Zero-Gravity Shooter/Assets/Scripts/WeaponFire.cs
@@ -8,12 +8,19 @@ public class WeaponFire : MonoBehaviour
 
 
     int shotsFired = 0;             //amount of shots fired
-    int maxFired = 33;              //amount that can be fired
+    int maxFired = 1;              //amount that can be fired
 
     private Rigidbody player;
-    public float bulletSpeed = 100; //will be private, public for easy testing
+    public float bulletSpeed = 25; //will be private, public for easy testing
   //public float fireRate = 0;      //private
     public float weaponRange = 50;  //private
+
+
+    public float force = 50.0f;
+    public float radius = 5.0f;
+    public float upwardsModifier = 0.0f;
+    public ForceMode forceMode;
+
 
     void Start()
     {
@@ -31,10 +38,20 @@ public class WeaponFire : MonoBehaviour
 
             shotsFired++;
 
+            if (Input.GetMouseButtonDown(1))
+            {
+                foreach (Collider col in Physics.OverlapSphere(transform.position, radius))
+                {
+                    if (col.GetComponent<Rigidbody>() != null)
+                    {
+                        col.GetComponent<Rigidbody>().AddExplosionForce(force, transform.position, radius, upwardsModifier, forceMode);
+                    }
+                }
+            }
         }
         else if (Input.GetKeyDown(KeyCode.R))
         {
             shotsFired = 0;
         }
-	}
+    }
 }
